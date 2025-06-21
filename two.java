d114fb2ec3866f069893ca307e42c2e2cbf11958
SaiprasathRam <81090638+SaiprasathRam@users.noreply.github.com>
2025-06-21 22:02:32 +0530

    Hello World program

---
+++ two.java
@@ -0,0 +1,21 @@
+package one;
+
+public class two {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Production server - jenkins
+		System.out.print("Smrithi");
+		//System is the package
+		//The main method name and class name should be same
+		//javac and java - compiler and interpreter present in JVM (Java Virtual Machine)
+		//Java is platform independent because of the JVM
+		//The recent version of JDK should be taken into consideration by us while compiling
+		//The environmental variable to be configured in the run time environment
+		//set path=%path%;path="C:\\Program Files\\Java\\jdk1.8.0_161\\bin";
+		//JRE - Java Runtime Environment
+		//JDK- contains information about the code
+		//JavaTPoint
+	}
+
+}
