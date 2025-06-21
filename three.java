f8ba9e29320a3d4c628aef0348b53d485d71fd6d
SaiprasathRam <81090638+SaiprasathRam@users.noreply.github.com>
2025-06-21 22:03:35 +0530

    Arithmetic operations

---
+++ three.java
@@ -0,0 +1,33 @@
+package one;
+
+public class three {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		int a,b,c;
+		double d,e;
+		a=5; b=6;
+		c=a+b;
+		System.out.println("a+b = ");
+		System.out.println(c);
+		c=a-b;
+		System.out.println("a-b = ");
+		System.out.println(c);
+		c=a*b;
+		System.out.println("a*b = ");
+		System.out.println(c);
+		c=b/a;
+		System.out.println("b/a = ");
+		System.out.println(c); 
+		d=7.4;
+		e=8.5;
+		e=d*e;
+		System.out.println("Double d*e");
+		System.out.println(e);
+		
+		
+
+	}
+
+}
