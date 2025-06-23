a243876089fffc19d98a63905caa30fa84fba660
SaiprasathRam <81090638+SaiprasathRam@users.noreply.github.com>
2025-06-23 21:39:04 +0530

    Argument with return type

---
+++ seven.java
@@ -0,0 +1,22 @@
+package one;
+//Category 4: Argument with return type
+public class seven {
+	  public int add(int a, int b)
+	  {
+	 
+	    int c=a+b;
+	    return c;
+	    
+	  }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    seven o=new seven();
+	    int d=o.add(5,5);
+	    System.out.println(d);
+	   
+	    
+
+	}
+
+}
