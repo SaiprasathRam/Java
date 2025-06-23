1fa02ddb25261bdca456cec18a24542e81331f47
SaiprasathRam <81090638+SaiprasathRam@users.noreply.github.com>
2025-06-23 21:37:41 +0530

    Method - With return type No argument

---
+++ six.java
@@ -0,0 +1,19 @@
+package one;
+//Category 3: With return type No argument
+public class six {
+	public int add()
+	  {
+	    int a=3, b=5;
+	    int c=a+b;
+	    return c;
+	    
+	  }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    six o=new six();
+	    int d=o.add();
+	    System.out.println(d);
+
+	}
+
+}
